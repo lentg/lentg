9571b8833d4ce0b3d1e2c360dc5b7538e90547a4
lentg <lentg@qq.com>
2011-03-16 17:38:30 +0800

    1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lentg::Application.config.secret_token = '00d9fb88f6ef41b9ce1b732f7da3789ff1de2920ba14f33950445dd5ca8f509e6bea1b44c778dcabc4da8efb699aa5d0cecc125d9619dd3c2eaa38b5c7ccd4df'
